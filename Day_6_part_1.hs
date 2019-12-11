d9fd1707cdb531d454c648e53d0e30ee0d5ad039
Jakub Kazimierski <jakubkazimierski622@gmail.com>
2019-12-11 18:11:38 +0100

    Add solution to day 6 problem part 1.

---
+++ Day_6_part_1.hs
@@ -0,0 +1,20 @@
+import Data.List
+import Data.List.Split
+import qualified Data.Map as M
+import Debug.Trace
+
+main :: IO ()
+main = interact solution
+
+solution :: String -> String
+solution = show . answer . map ((\[x, y] -> (x, y)) . splitOn ")") . lines
+
+answer :: [(String, String)] -> Integer
+answer = foldr (+) 0 . orbitPaths
+
+orbitPaths :: [(String, String)] -> M.Map String Integer 
+orbitPaths m = M.fromList $ map (\(p@(x, y)) -> (y, orbitPath m p)) m
+
+orbitPath :: [(String, String)] -> (String, String) -> Integer
+orbitPath m ("COM", _) = 1
+orbitPath m (x, y)     = (+1) $ orbitPath m $ head $ filter (\(_, a) -> a == x) m
