1e0085702e79c25d6fd616b3bde0b76aa40372a6
Jakub Kazimierski <jakubkazimierski622@gmail.com>
2019-12-11 21:15:22 +0100

    Add solution to day 6 problem part 2.

---
+++ Day_6_part_2.hs
@@ -0,0 +1,21 @@
+import Data.List
+import Data.List.Split
+import qualified Data.Set as S
+import Debug.Trace
+
+main :: IO ()
+main = interact solution
+
+solution :: String -> String
+solution = show . answer . map ((\[x, y] -> (x, y)) . splitOn ")") . lines
+
+answer :: [(String, String)] -> Integer
+answer m = (fromIntegral . (+ (-2)) . S.size . foldr (\x y -> S.union x y S.\\ S.intersection x y) S.empty . orbitPaths m)
+  $ filter (\(_, x) -> x == "YOU" || x == "SAN") m
+
+orbitPaths :: [(String, String)] -> [(String, String)] -> [S.Set (String, String)] 
+orbitPaths m s = map S.fromList $ map (\(p@(x, y)) -> orbitPath m p) s
+
+orbitPath :: [(String, String)] -> (String, String) -> [(String, String)] 
+orbitPath m o@("COM", _) = [o]
+orbitPath m o@(x, _)     = o:(orbitPath m $ head $ filter (\(_, a) -> a == x) m)
