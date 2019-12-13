4b19a6b4ecd08f8d440a7e89757767f5b5321975
Jakub Kazimierski <jakubkazimierski622@gmail.com>
2019-12-13 18:45:58 +0100

    Add solution to day 10 problem part 1.

---
+++ Day_10/Day_10_part_1.hs
@@ -0,0 +1,34 @@
+import Data.List
+import qualified Data.Set as S
+
+type Vector = (Int, Int)
+data Key = NX | X | ANGLE Double | ITSELF
+  deriving (Eq, Ord)
+
+main :: IO ()
+main = interact solution
+
+solution :: String -> String
+solution = show . maximum 
+  . (\m -> map (observableAsteroids m) (allAsteroids m))
+  . lines
+
+observableAsteroids :: [String] -> Vector -> Int
+observableAsteroids m v =
+  (length . S.filter (/= ITSELF) . S.fromList . map (calculateKey v))
+  $ allAsteroids m  
+
+calculateKey :: Vector -> Vector -> Key
+calculateKey (x, y) (a, b) =
+  case r of
+    (rx, 0)  ->
+      if rx > 0 then X  else
+      if rx < 0 then NX else ITSELF
+    (rx, ry) -> ANGLE (atan2 (fromIntegral rx) (fromIntegral ry))
+  where r = (x - a, y - b)
+
+allAsteroids :: [String] -> [Vector]
+allAsteroids m = [(x, y) |
+                  x <- [0..(length m - 1)],
+                  y <- [0..(length (head m) - 1)],
+                  ((m!!y)!!x) == '#']
